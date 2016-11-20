b6d43804be792e2ade3243eb5ebe3a0a4cd731b0
Kevin Conter <KevinConter>
2016-11-20 18:14:29 +0100

    Implementazione della gestione delle richieste e Refining della libreria.

--- index.js
+++ index.js
@@ -18,13 +18,16 @@ app.set('port', (process.env.PORT || 8848));
 //set the server to respond to a file request
 app.use('/files',express.static(__dirname+'/public'));
 
+
 //Server Manager
 //use for GET requests
 app.get('/', function(request, response) 
 {
 	bind.toFile(
 		'tpl/form.tpl',
-		{},
+		{
+			finded: false
+		},
 		function(data){
 			response.writeHead(200, {'Content-Type': 'text/html'});
 			response.end(data);
@@ -36,55 +39,126 @@ app.get('/', function(request, response)
 app.post(bodyParser.urlencoded({ extended: false }));
 
 //use for POST requests of delete
-app.post('/delete', function(request, response) 
-{
-    //Set header for the response
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	/*var text = '';
-
-	if ( typeof request.body !== 'undefined' && request.body){
-        //the content of the POST receiced
-		text = "request.body: " + util.inspect(request.body) + "\n";
-		
-        //content of the post
-		var username;
-		var password;
-		
-		//if query is defined and not null
-		if ( typeof request.body.username !== 'undefined' && request.body.username)
-            //save content of username
-			username = request.body.username;
-		else 
-			username = "not defined";
-		
-		if ( typeof request.body.password !== 'undefined' && request.body.password)
-            //save content of password
-    		password = request.body.password;
-		else 
-			password = "not defined";
-			    	
-        text = text + "post received: " + username + ", "+ password;
-	}
-	else
-	{
-		text = "body undefined";
+app.post('/delete', function(request, response){
+	if(typeof request.body !== 'undefined' && request.body){
+		if(typeof request.body.iSD !== 'undefined' && request.body.iSD){
+			var idEmployee = parseInt(request.body.iSD);
+			myLib.removeEmployee(idEmployee);
+		}else{
+			console.log("iSD field not defined");
+		}
+	}else{
+		console.log("Request body not defined");
 	}
 
-    console.log(text);
-    
-    //answer a JSON file
-	var otherArray = ["item1", "item2"];
-	var otherObject = { item1: "item1val", item2: "item2val" };
-    
-	var json = JSON.stringify({ 
-    	anObject: otherObject, 
-	    anArray: otherArray, 
-    	another: "item"
-	});
-    
-    //send JSON
-    response.end(json);
-	*/
+	bind.toFile(
+		'tpl/form.tpl',
+		{
+			finded: false
+		},
+		function(data){
+			response.writeHead(200,{'Content-Type':'text/html'});
+			reponse.end(data);
+		}
+	);
+});
+
+app.post('/search',function(request,response){
+	if(typeof request.body !== 'undefined' && request.body){
+		if(typeof request.body.iSD !== 'undefined' && request.body.iSD){
+			var idEmployee = parseInt(request.body.iSD);
+			var employee = myLib.getEmployee(idEmployee);
+			if(employee=={}){
+				bind.toFile(
+					'tpl/form.tpl',
+					{
+						finded: true
+					},
+					function(data){
+						response.writeHead(200,{'Content-Type':'text/html'});
+						response.end(data);
+					}
+				);
+			}else{
+				bind.toFile(
+					'tpl/form.tpl',
+					{
+						finded: true,
+						id: employee.id,
+						name: employee.name,
+						surname: employee.surname,
+						level: employee.level,
+						salary: employee.salary
+					},
+					function(data){
+						response.writeHead(200,{'Content-Type':'text/html'});
+						response.end(data);
+					}
+				);
+			}
+		}else{
+			console.log("iSD field not defined");
+		}
+	}else{
+		console.log("Request body not defined");
+	}
+});
+
+app.post('/insert',function(request,response){
+	if(typeof request.body !== 'undefined' && request.body){
+		var employee = myLib.Employee(-1,"","",-1,-1);
+		var error = false;
+		if(typeof request.body.iID !== 'undefined' && request.body.iID){
+			var id = request.body.iID;
+			if(id==""){
+				employee.id=myLib.getNextId();
+			}else{
+				employee.id=parseInt(id);
+			}
+		}
+		else{
+			error = true;
+		}
+		if(typeof request.body.iName !== 'undefined' && request.body.iName){
+			employee.name=request.body.iName;
+		else{
+			error = true;
+		}
+		if(typeof request.body.iSurname !== 'undefined' && request.body.iSurname){
+			employee.surname=request.body.iSurname;
+		}else{
+			error = true;
+		}
+		if(typeof request.body.level !== 'undefined' && request.body.level){
+			employee.level=parseInt(request.body.level);
+		}else{
+			error = true;
+		}
+		if(typeof request.body.salary !== 'undefined' && request.body.salary){
+			employee.salary=parseInt(request.body.salary);
+		}else{
+			error = true;
+		}
+		if(error){
+			response.writeHead(409);
+			response.end("Incorrect data sent with the request");
+		}else{
+			myLib.addEmployee(employee);
+			bind.toFile(
+				'tpl/form.tpl',
+				{
+					finded: false
+				},
+				function(data){
+					response.writeHead(200,{'Content-Type':'text/html'});
+					reponse.end(data);
+				}
+			);
+		}
+			
+	}else{
+		console.log("Request body not defined");
+	}
 });
 
 app.listen('8848','127.0.0.1');
